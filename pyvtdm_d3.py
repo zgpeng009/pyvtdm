877c9c2de5a7059f66450ecf03ab60e23259f841
rainf-pzg-PC <pzg2821003982@gmail.com>
2019-09-16 11:22:48 +0800

    20190916

--- pyvtdm_d3.py
+++ pyvtdm_d3.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
 import sys
+import os
 import numpy as np
+import matplotlib
 
 from PyQt5.uic import loadUiType
 from PyQt5.QtCore import *
@@ -9,6 +11,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 Ui_pyvtdm = loadUiType('pyvtdm_d3.ui')[0]
+myfont = matplotlib.font_manager.FontProperties(fname='simhei.ttf')
 
 
 class DIALOG2(QDialog, Ui_pyvtdm):
@@ -17,7 +20,17 @@ class DIALOG2(QDialog, Ui_pyvtdm):
         self.setupUi(self)
         self.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinMaxButtonsHint)
 
+        names = [u'静力试验', u'加载级', u'位移']
+        data1 = [1, 2, 3]
+        data2 = [2, 4, 6]
         self.axes = self.widget.fig.add_subplot(111)
+        self.axes.grid(True)
+        self.axes.set_title(names[0], fontproperties=myfont, fontsize=15)
+        self.axes.set_xlabel(names[1], fontproperties=myfont, fontsize=12)
+        self.axes.set_ylabel(names[2], fontproperties=myfont, fontsize=12)
+        self.axes.plot(data1, data1, 'k--', linewidth=0.7)
+        self.axes.scatter(data1, data2, s=40, c='', marker='o', edgecolor='c')
+        self.axes.legend(loc='upper left')
 
 
 if __name__ == '__main__':
